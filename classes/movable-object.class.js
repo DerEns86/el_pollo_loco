7879885b64ca4a65592a77947f188699cb8238af
Viktor Ens <v.ens86@yahoo.de>
2023-10-07 16:49:00 +0200

    gravitation added

--- classes/movable-object.class.js
+++ classes/movable-object.class.js
@@ -8,13 +8,30 @@ class MovableObject {
     imageCache = {};
     currentImage = 0;
     otherDirection = false;
+    speedY = 0;
+    acceleration = 1.5;
+
+
+
+    applyGravity() {
+        setInterval(() => {
+            if (this.isaboveGround()) {
+                this.y -= this.speedY;
+                this.speedY -= this.acceleration;
+            }
+        }, 1000 / 25);
+    }
+
+    isaboveGround() {
+        return this.y < 230;
+    }
 
 
     loadImage(path) {
         this.img = new Image() // Image() ist eine vordefenierte Methode von JS ->this.img = document.getElementById('image') <img id= "image" src>
         this.img.src = path;
     }
-    
+
     loadImages(arr) {
         arr.forEach((path) => {
 
@@ -31,7 +48,7 @@ class MovableObject {
 
     moveLeft() {
         {
-            setInterval( () => {
+            setInterval(() => {
                 this.x -= this.speed;
             }, 1000 / 60);
         }
@@ -39,8 +56,8 @@ class MovableObject {
 
     playAnimation(images) {
         let i = this.currentImage % this.IMAGES_WALKING.length;
-                let path = images[i];
-                this.img = this.imageCache[path];
-                this.currentImage++;
+        let path = images[i];
+        this.img = this.imageCache[path];
+        this.currentImage++;
     }
 }
