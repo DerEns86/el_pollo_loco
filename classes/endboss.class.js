67521bae7fc5808851402661f2758da4dfe417d0
Viktor Ens <v.ens86@yahoo.de>
2023-10-13 21:07:47 +0200

    fix

--- classes/endboss.class.js
+++ classes/endboss.class.js
@@ -3,7 +3,7 @@ class Endboss extends MovableObject {
     height = 400;
     width = 400;
     y= 50;
-
+    
     IMAGES_WALKING = [
         'img/4_enemie_boss_chicken/2_alert/G5.png',
         'img/4_enemie_boss_chicken/2_alert/G6.png',
