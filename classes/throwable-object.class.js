1e2e32bfb0e66c624c3fd2b8f44c1470b64418e1
Viktor Ens <v.ens86@yahoo.de>
2023-10-22 11:26:57 +0200

    throw bottles small changes

--- classes/throwable-object.class.js
+++ classes/throwable-object.class.js
@@ -1,7 +1,26 @@
 class ThrowableObject extends MovableObject {
 
+    IMAGES_ROTATE = [
+        'img/6_salsa_bottle/bottle_rotation/1_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/2_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/3_bottle_rotation.png',
+        'img/6_salsa_bottle/bottle_rotation/4_bottle_rotation.png',
+    ];
+
+    IMAGES_SPLASH = [
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/1_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/2_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/3_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/4_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/5_bottle_splash.png',
+        'img/6_salsa_bottle/bottle_rotation/bottle_splash/6_bottle_splash.png',
+    ]
+
+
     constructor(x, y) {
         super().loadImage('img/6_salsa_bottle/salsa_bottle.png');
+        this.loadImages(this.IMAGES_ROTATE);
+        this.loadImages(this.IMAGES_SPLASH);
         this.x = x;
         this.y = y;
         this.height = 50;
@@ -10,10 +29,22 @@ class ThrowableObject extends MovableObject {
     }
 
     throw() {
+        // TODO abändern mit if-else. die playAnimation Splash stoppen oder die 
+        // bottle splicen
         this.speedY = 20;
         this.applyGravity();
-        setInterval(() =>{
-            this.x += 10;
-        }, 50)
+        
+            setInterval(() => {
+               
+               if (this.y <= 365) {
+                this.x += 10;
+                this.playAnimation(this.IMAGES_ROTATE);
+             }else {
+            this.playAnimation(this.IMAGES_SPLASH);
+            this.y = 365;
+        }
+            
+            }, 50)
+       
     }
 }
